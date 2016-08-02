efbde623a62795166fd1ba053907ea47e254aab7
Kuppusamy <mkuppus1@MGC000001980.itek.ford.com>
2016-08-02 15:12:02 -0400

    Add some changes

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -23,6 +23,7 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"Memory issue has been fixed 1.1");
 }
 
 @end
